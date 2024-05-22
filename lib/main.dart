1434e709f2c70c433bb44c11e99f331b1e9e0e01
Rafy Bhuiyan <rafybhuiyan55@gmail.com>
2024-05-23 01:13:09 +0600

    removed the debugging tag and added a themedata for the future asset

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Poppins',
+      ),
     );
   }
 }
