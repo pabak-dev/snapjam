0e9cb3a55aa17238e73c5a5a79790a312eb47a54
Rafy Bhuiyan <rafybhuiyan55@gmail.com>
2024-05-23 01:27:36 +0600

    created color constants

---
+++ lib/constants/Constantcolors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Constantcolors{
+  final Color lightColor = Color(0xff6c788a);
+  final Color DarkColor = Color(0xff100E20);
+  final Color blueColor = Colors.lightBlueAccent.shade400;
+  final Color LightBlueColor = Colors.lightBlueAccent.shade200;
+  final Color redcolor  =  Colors.red;
+  final Color whitecolor = Colors.white;
+  final Color bluegreycolor = Colors.blueGrey.shade900;
+  final Color greenColor = Colors.greenAccent;
+  final Color yellowColor =  Colors.yellow;
+  final Color greyColor = Colors.grey.shade600;
+}
