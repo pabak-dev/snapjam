366a2ff58904b14a56c6bf07e171dc46e39a5400
Pruz0 <53967227+Pruz0@users.noreply.github.com>
2024-05-23 01:47:15 +0600

    Import font and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:snapjam/SplashScreen.dart';
+import 'package:snapjam/constants/Constantcolors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,11 +10,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    Constantcolors cc = Constantcolors();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'Poppins',
+        fontFamily: 'Freeman',
+        secondaryHeaderColor: cc.blueColor,
+        canvasColor: Colors.transparent,
       ),
+      home: SplashScreen(),
     );
   }
 }
