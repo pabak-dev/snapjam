1fc37c2cee1722dfff041029a1b7d6b1473b0244
Rafy Bhuiyan <rafybhuiyan55@gmail.com>
2024-05-23 02:13:25 +0600

    upgraded dependencies and some changed

--- lib/screens/Landingpage/LandingPage.dart
+++ lib/screens/Landingpage/LandingPage.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:snapjam/constants/Constantcolors.dart';
 class LandingPage extends StatelessWidget {
-  Constantcolors cc = Constantcolors();
+  final Constantcolors cc = Constantcolors();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: cc.whiteColor,
+
     );
   }
 }
